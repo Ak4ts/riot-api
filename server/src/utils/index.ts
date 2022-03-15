9b0cd732fe7b048a6931c2dc2d03ba329097e363
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-03-15 18:25:01 -0300

    feat: added regions on utils

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -4,6 +4,7 @@ require("dotenv").config()
 export const api = axios.create({
   headers: {
     "X-Riot-Token": process.env.API_KEY,
-  },
-  baseURL: "https://br1.api.riotgames.com",
+  }
 });
+
+export const regions = ["br1", "eun1", "euw1", "jp1", "kr", "la1", "la2", "na1", "oc1", "ru", "tr1"]
