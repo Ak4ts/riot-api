c317319b3cca51da2a73c7b7a9dc52b299ba100c
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-03-15 18:25:35 -0300

    feat: added summoner route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import ChampionV3 from "../controllers/Champion-V3";
+import Champions from "../controllers/Champion-V3";
+import Summoners from "../controllers/Summoner-V4"
 
 const routes = Router();
 
-routes.get("/api/v1/freeweek", ChampionV3.v3)
-
+routes.get("/api/v1/freeweek", Champions.v3)
+routes.get("/api/v1/summoner", Summoners.searchByName)
 export default routes;
